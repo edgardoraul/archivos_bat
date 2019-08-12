2957a73b6071b9b7ad1e2924bd3b42168bbeed89
WebModerna <edgardogalletto@gmail.com>
2019-08-12 17:26:24 -0300

    Copia de un archivo a la otra carpeta integrada.
    Consiste en el comando automatizado de copia del archivo en cuestión a la carpeta sincronizada de Producteca.

--- Renombrador.bat
+++ Renombrador.bat
@@ -5,3 +5,4 @@ rename D:\Dropbox\Aplicaciones\Producteca\price*.* price.*
 rename D:\Dropbox\Aplicaciones\Producteca\stock*.* stock.*
 rename D:\Dropbox\TUCUMAN\stock*.* stock_tucuman.*
 del /S /F /Q D:\Dropbox\TUCUMAN\price*.*
+copy D:\Dropbox\TUCUMAN\stock_tucuman.* D:\Dropbox\Aplicaciones\Producteca\stock_tucuman.*
