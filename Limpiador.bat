f11becdbc33c02dc5e04580e5685cf5993458074
WebModerna <edgardogalletto@gmail.com>
2020-04-20 15:46:35 -0300

    Agregado de sucursal Movil 5 y Movil2
    Eliminación de archivos y renombrador de los mismos.
    Nuevas rutas a las carpetas creadas.

--- Limpiador.bat
+++ Limpiador.bat
@@ -2,3 +2,5 @@
 rem "Elimina los archivos viejos para Producteca."
 del /S /F /Q D:\Dropbox\Aplicaciones\Producteca\*.*
 del /S /F /Q D:\Dropbox\TUCUMAN\*.*
+del /S /F /Q D:\Dropbox\MOVIL2\*.*
+del /S /F /Q D:\Dropbox\MOVIL5\*.*
--- Renombrador.bat
+++ Renombrador.bat
@@ -6,3 +6,11 @@ rename D:\Dropbox\Aplicaciones\Producteca\stock*.* stock.*
 rename D:\Dropbox\TUCUMAN\stock*.* stock_tucuman.*
 del /S /F /Q D:\Dropbox\TUCUMAN\price*.*
 copy D:\Dropbox\TUCUMAN\stock_tucuman.* D:\Dropbox\Aplicaciones\Producteca\stock_tucuman.*
+
+rename D:\Dropbox\MOVIL5\stock*.* stock_movil5.*
+del /S /F /Q D:\Dropbox\MOVIL5\price*.*
+copy D:\Dropbox\MOVIL5\stock_movil5.* D:\Dropbox\Aplicaciones\Producteca\stock_movil5.*
+
+rename D:\Dropbox\MOVIL2\stock*.* stock_movil2.*
+del /S /F /Q D:\Dropbox\MOVIL2\price*.*
+copy D:\Dropbox\MOVIL2\stock_movil2.* D:\Dropbox\Aplicaciones\Producteca\stock_movil2.*
