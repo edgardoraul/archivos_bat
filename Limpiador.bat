f0cf7090ecf3ebd727472ce8fef4e2ccd07c9cad
WebModerna <edgardogalletto@gmail.com>
2020-04-29 11:19:55 -0300

    Agragada Neuquén

--- Limpiador.bat
+++ Limpiador.bat
@@ -4,3 +4,4 @@ del /S /F /Q D:\Dropbox\Aplicaciones\Producteca\*.*
 del /S /F /Q D:\Dropbox\TUCUMAN\*.*
 del /S /F /Q D:\Dropbox\MOVIL2\*.*
 del /S /F /Q D:\Dropbox\MOVIL5\*.*
+del /S /F /Q D:\Dropbox\NEUQUEN\*.*
--- Renombrador.bat
+++ Renombrador.bat
@@ -3,6 +3,7 @@ rem "Renombra los archivos para que tengan siempre el mismo nombre."
 
 rename D:\Dropbox\Aplicaciones\Producteca\price*.* price.*
 rename D:\Dropbox\Aplicaciones\Producteca\stock*.* stock.*
+
 rename D:\Dropbox\TUCUMAN\stock*.* stock_tucuman.*
 del /S /F /Q D:\Dropbox\TUCUMAN\price*.*
 copy D:\Dropbox\TUCUMAN\stock_tucuman.* D:\Dropbox\Aplicaciones\Producteca\stock_tucuman.*
@@ -14,3 +15,7 @@ copy D:\Dropbox\MOVIL5\stock_movil5.* D:\Dropbox\Aplicaciones\Producteca\stock_m
 rename D:\Dropbox\MOVIL2\stock*.* stock_movil2.*
 del /S /F /Q D:\Dropbox\MOVIL2\price*.*
 copy D:\Dropbox\MOVIL2\stock_movil2.* D:\Dropbox\Aplicaciones\Producteca\stock_movil2.*
+
+rename D:\Dropbox\NEUQUEN\stock*.* stock_neuquen.*
+del /S /F /Q D:\Dropbox\NEUQUEN\price*.*
+copy D:\Dropbox\NEUQUEN\stock_neuquen.* D:\Dropbox\Aplicaciones\Producteca\stock_neuquen.*
