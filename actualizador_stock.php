f22894b58e60225cabd1070ef09ef70da530781f
Edgard_Roul <edgardo_raul@hotmail.com>
2020-10-22 17:58:42 -0300

    tiene un error
    No puede colocar el total disponible del producto que tiene combinaciones. Si tiene 3 de una variante y 4 de otra, debería poner 7. Pero no lo hace.

--- actualizador_stock.php
+++ actualizador_stock.php
@@ -9,16 +9,16 @@ include "/home/rerda/public_html/config/settings.inc.php";
 $con = @mysqli_connect( _DB_SERVER_, _DB_USER_, _DB_PASSWD_, _DB_NAME_ );
 
 // Importando el archivos
-$info = fopen ("/home/rerda/public_html/upload/stock.csv" , "r" );
+$info = fopen ( "/home/rerda/public_html/upload/stock.csv" , "r" );
 while ( ( $datos = fgetcsv( $info, 10000, ",") ) !== FALSE )
 {
-	$linea[] = array('Referencia'=>$datos[0],'Cantidad'=>$datos[1]);
+	$linea[] = array( 'Referencia' => $datos[0], 'Cantidad' => $datos[1] );
 }
-fclose ($info);
+fclose ( $info );
 
-$insertados = 0;
-$errores = 0;
-$actualizados = 0;
+$insertados		= 0;
+$errores 		= 0;
+$actualizados 	= 0;
 
 // Vaciamos la tabla antes que nada
 mysqli_query( $con, "TRUNCATE TABLE importacion_stock;" );
@@ -32,12 +32,12 @@ foreach($linea as $indice=>$value)
 
 
 	//Creamos la sentencia SQL y la ejecutamos
-	$sql = mysqli_query( $con, "SELECT * FROM importacion_stock WHERE Referencia = '$codigo'");
+	$sql = mysqli_query( $con, "SELECT * FROM importacion_stock WHERE Referencia = '$codigo'" );
 	$num = mysqli_num_rows( $sql );
 
 	if ( $num == 0 )
 	{
-		$sql="INSERT INTO importacion_stock (Referencia, Cantidad) VALUES('$codigo', '$campo2')";
+		$sql = "INSERT INTO importacion_stock (Referencia, Cantidad) VALUES( '$codigo', '$campo2' )";
 		
 		if ( $insert = mysqli_query( $con, $sql ) )
 		{
@@ -64,12 +64,12 @@ foreach($linea as $indice=>$value)
 }
 /*
 	Esta dos consultas van a generar un error con stock de más o de menos en las publicaciones que tienen combinaciones. Pero en el resto se supone que no.
-*/
+
 // Primera consulta
-mysqli_query( $con, "UPDATE ps_product, importacion_stock SET quantity = `importacion_stock`.`Cantidad` WHERE `ps_product`.`reference` = `importacion_stock`.`Referencia`");
+mysqli_query( $con, "UPDATE ps_product, importacion_stock SET `ps_product`.`quantity` = 1 WHERE `ps_product`.`reference` = `importacion_stock`.`Referencia`" );
 
 // Segunda consulta
-mysqli_query( $con, "UPDATE ps_stock_available, ps_product SET `ps_stock_available`.`quantity` = `ps_product`.`quantity` WHERE `ps_stock_available`.`id_product` = `ps_product`.`id_product`");
+mysqli_query( $con, "UPDATE ps_stock_available, ps_product SET `ps_stock_available`.`quantity` = `ps_product`.`quantity` WHERE `ps_stock_available`.`id_product` = `ps_product`.`id_product`" );
 
 /* 
 	Estas consultas corrigen los errores arrastrados por las dos consultas anteriores,
@@ -77,14 +77,14 @@ mysqli_query( $con, "UPDATE ps_stock_available, ps_product SET `ps_stock_availab
 */
 
 // Tercera consulta
-mysqli_query( $con, "UPDATE ps_product_attribute, importacion_stock SET quantity = `importacion_stock`.`Cantidad` WHERE `ps_product_attribute`.`reference` = `importacion_stock`.`Referencia`");
+mysqli_query( $con, "UPDATE ps_product_attribute, importacion_stock SET `ps_product_attribute`.`quantity` = `importacion_stock`.`Cantidad` WHERE `ps_product_attribute`.`reference` = `importacion_stock`.`Referencia`" );
 
 // Cuarta constulta
-mysqli_query( $con, "UPDATE ps_stock_available, ps_product_attribute SET `ps_stock_available`.`quantity` = `ps_product_attribute`.`quantity` WHERE `ps_stock_available`.`id_product_attribute` = `ps_product_attribute`.`id_product_attribute`");
+mysqli_query( $con, "UPDATE ps_stock_available, ps_product_attribute SET `ps_stock_available`.`quantity` = `ps_product_attribute`.`quantity` WHERE `ps_stock_available`.`id_product_attribute` = `ps_product_attribute`.`id_product_attribute`" );
 
 // Control de salida
 echo "Registros insertados: " . number_format( $insertados, 2 ) . " <br/>";
-echo "Registros actualizados: "  .number_format( $actualizados, 2 ) . " <br/>";
+echo "Registros actualizados: " .number_format( $actualizados, 2 ) . " <br/>";
 echo "Errores: " . number_format( $errores, 2 ) . " <br/>";
 
 ?>
