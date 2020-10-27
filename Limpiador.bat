00547f888f10b4521ced3519238d322e8b37e8f4
Edgard_Roul <edgardo_raul@hotmail.com>
2020-10-27 12:03:53 -0300

    Actualización

--- Limpiador.bat
+++ Limpiador.bat
@@ -5,3 +5,6 @@ del /S /F /Q D:\Dropbox\TUCUMAN\*.*
 del /S /F /Q D:\Dropbox\MOVIL2\*.*
 del /S /F /Q D:\Dropbox\MOVIL5\*.*
 del /S /F /Q D:\Dropbox\NEUQUEN\*.*
+
+del /S /F /Q C:\Users\EDGARD\AppData\Local\Temp\*.*
+del /S /F /Q C:\Windows\Temp\*.*
