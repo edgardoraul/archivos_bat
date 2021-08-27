e008e92b385ac39d4c089116e86eece837bf4472
Edgard_Roul <edgardo_raul@hotmail.com>
2021-08-27 17:00:59 -0300

    Corrección del script
    No tengo idea de qué hice aquí, pero algo seguro.

--- facebookpixel.js
+++ facebookpixel.js
@@ -1,9 +1,9 @@
 // Comprueba que se haya cargado JQuery y la carga total de la web
-jQuery(document).ready( function()
+jQuery( document ).ready( function()
 {
 	/******* Corrige el error del flete gratis ***********+*/
-	const varA = $(".ajax_cart_shipping_cost .unvisible");
-	varA.html("A determinar");
+	const varA = $( '.ajax_cart_shipping_cost .unvisible' );
+	varA.html( 'A determinar' );
 
 	/************* El Facebook Pixel **************/
 	!function(f,b,e,v,n,t,s)
@@ -23,47 +23,47 @@ jQuery(document).ready( function()
 		t.src = v;
 		s = b.getElementsByTagName(e)[0];
 		s.parentNode.insertBefore(t,s)
-	} (window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+	} ( window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js' );
 
-	fbq('init', '1335485450135411');
-	fbq('track', 'PageView');
+	fbq( 'init', '1335485450135411' );
+	fbq( 'track', 'PageView' );
 
 	// Control de la carga de la web
-	console.log("La página cargó.");
+	console.log( 'La página cargó.' );
 
 	// Definiendo la variable
-	let totalCarrito = $(".price.cart_block_total.ajax_block_cart_total").html().replace("$ ", "").replace(".", "").replace(",", ".").replace(" ", "");
-	totalCarrito = parseFloat(totalCarrito).toFixed(2) * 1;
+	let totalCarrito = $( '.price.cart_block_total.ajax_block_cart_total' ).html().replace( '$ ', '' ).replace( '.', '' ).replace( ',', '.' ).replace( ' ', '' );
+	totalCarrito = parseFloat( totalCarrito ).toFixed( 2 ) * 1;
 
 
 	// Validando sólo si hay 
-	if ( $(".price.cart_block_total.ajax_block_cart_total").html() !== "" )
+	if ( $( '.price.cart_block_total.ajax_block_cart_total').html() != '' )
 	{
 		// Monto total anterior del carrito de compras. Convertido a número decimal
-		console.log( `totalCarrito es ${ typeof(totalCarrito) } = ${totalCarrito} `);
+		console.log( `totalCarrito es ${ typeof( totalCarrito ) } = ${ totalCarrito } `);
 		
 		// Comprobando si hubo algún cambio o actualización de los valores
-		$(".price.cart_block_total.ajax_block_cart_total").on("change", () =>
+		$( '.price.cart_block_total.ajax_block_cart_total' ).on( 'change', () =>
 		{
-			totalCarrito = $(".price.cart_block_total.ajax_block_cart_total").html().replace("$ ", "").replace(".", "").replace(",", ".").replace(" ", "");
-			totalCarrito = parseFloat(totalCarrito).toFixed(2) * 1;
+			totalCarrito = $( '.price.cart_block_total.ajax_block_cart_total' ).html().replace( '$ ', '' ).replace( '.', '' ).replace( ',', '.' ).replace( ' ', '' );
+			totalCarrito = parseFloat( totalCarrito ).toFixed( 2 ) * 1;
 			categoria();
 		});
 	}
 
 	// Muestra en consola
-	function mostrarConsola(pre, cant, carr)
+	function mostrarConsola( pre, cant, carr )
 	{
-		console.log( `Precio es ${typeof(pre)} = ${pre}` );
-		console.log( `Cantidad es ${typeof(cant)} = ${cant}` );
-		console.log( `Agregado al Carrito por ${carr}` );
+		console.log( `Precio es ${ typeof(pre) } = ${ pre }` );
+		console.log( `Cantidad es ${ typeof(cant) } = ${ cant }` );
+		console.log( `Agregado al Carrito por ${ carr }` );
 	}
 
 	// Mostrar en el tracking del féisbuc
-	function husmeador(totalCarr, moneda)
+	function husmeador( totalCarr, moneda )
 	{
 		// Activa el evento del píxel féisbuc
-		fbq("track", "AddToCart",
+		fbq( 'track', 'AddToCart',
 		{
 			value: totalCarr,
 			currency: moneda
@@ -71,13 +71,13 @@ jQuery(document).ready( function()
 	}
 
 	// Cuando se está en la página de categorías
-	if ( $("#category").length > 0 )
+	if ( $( '#category' ).length > 0 )
 	{
-		// Cuando se hace click a "Agregar al Carrito"
-		$(".button.ajax_add_to_cart_button.btn.btn-default").on("click", () =>
+		// Cuando se hace click a 'Agregar al Carrito'
+		$('.button.ajax_add_to_cart_button.btn.btn-default').on('click', () =>
 		{
-			let precio = $(this).parent().prev().find(".price.product-price").html().replace("$ ", "").replace(".", "").replace(",", ".");
-			precio = parseFloat(precio).toFixed(2) * 1;
+			let precio = $( this ).parent().prev().find( '.price.product-price' ).html().replace( '$ ', '' ).replace( '.', '' ).replace( ',', '.');
+			precio = parseFloat( precio ).toFixed( 2 ) * 1;
 
 			// Convierte las cantidades a enteros
 			let cantidad = 1;
@@ -90,77 +90,102 @@ jQuery(document).ready( function()
 			mostrarConsola(precio, cantidad, totalCarrito);
 			
 			// Activa el evento del píxel féisbuc
-			husmeador(totalCarrito, "ARS");
+			husmeador( totalCarrito, 'ARS' );
 		});
 	}
 
 
 	// En la página del producto o en la home
-	if ( $("#product").length > 0 || $("#index").length > 0 )
+	if ( $( '#product' ).length > 0 || $( '#index' ).length > 0 )
 	{
-		// Cuando se hace click a "Agregar al Carrito"
-		$("#add_to_cart button, .ajax_add_to_cart_button").on("click", () =>
+		// Cuando se hace click a 'Agregar al Carrito'
+		$( '#add_to_cart button, .ajax_add_to_cart_button' ).on( 'click', () =>
 		{
 			// Convierte los precios a decimal
-			let precio = $("#our_price_display").attr("content").replace("$ ", "");
+			let precio = $( '#our_price_display' ).attr( 'content' ).replace( '$ ', '' );
 			precio = parseFloat(precio).toFixed(2);
 
 			// Convierte las cantidades a enteros
-			let cantidad = $("#quantity_wanted").val();
-			cantidad = parseInt(cantidad);
+			let cantidad = $( '#quantity_wanted' ).val();
+			cantidad = parseInt( cantidad );
 			
 			// Obtiene el total del carrito agrendo lo actual
 			let productosAgregando = precio * cantidad;
 			totalCarrito = totalCarrito + productosAgregando;
 
 			// Para monitorear que sea correcto.
-			mostrarConsola(precio, cantidad, totalCarrito);
+			mostrarConsola( precio, cantidad, totalCarrito );
 			
 			// Activa el evento del píxel féisbuc
-			husmeador(totalCarrito, "ARS");
+			husmeador( totalCarrito, 'ARS' );
 		});
 	}
 
 
 	// Cuando alguien inicia el proceso del pago, o ir hacia la caja.
-	if ( $("#order").length > 0 )
+	if ( $( '#order').length > 0 )
 	{
-		fbq('track', 'InitiateCheckout');
-		console.log("Comenzando checkout.");
+		fbq( 'track', 'InitiateCheckout' );
+		console.log( 'Comenzando checkout.' );
 	}
 
 	// Controla que sólo se cargue cuando finalize la compra.
-	if ( $("#order-confirmation").length > 0 )
+	if ( $( '#order-confirmation' ).length > 0 )
 	{
 		// Extrae la información de un script al final de la página.
-		const TotalCompra = window.dataLayer.find(monto => monto.transactionTotal);
+		const TotalCompra = window.dataLayer.find( monto => monto.transactionTotal );
 		TotalCompra2 = TotalCompra.transactionTotal;
-		console.log(`La compra ya está hecha por $ ${ TotalCompra2 }`);
+		console.log( `La compra ya está hecha por $ ${ TotalCompra2 }` );
 		
-		fbq("track", "Purchase",
+		fbq( 'track', 'Purchase',
 		{
 			value: TotalCompra2,
-			currency: "ARS"
+			currency: 'ARS'
+		});
+	}
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+	console.log('Probando si carga el evento');
+	
+	// Cuando alguien inicia el proceso del pago, o ir hacia la caja.
+	if ( $( '#order').length > 0 )
+	{
+		fbq( 'track', 'InitiateCheckout' );
+		console.log( 'Comenzando checkout.' );
+	}
+
+	// Controla que sólo se cargue cuando finalize la compra.
+	if ( $( '#order-confirmation' ).length > 0 )
+	{
+		// Extrae la información de un script al final de la página.
+		const TotalCompra = window.dataLayer.find( monto => monto.transactionTotal );
+		TotalCompra2 = TotalCompra.transactionTotal;
+		console.log( `La compra ya está hecha por $ ${ TotalCompra2 }` );
+		
+		fbq( 'track', 'Purchase',
+		{
+			value: TotalCompra2,
+			currency: 'ARS'
 		});
 	}
 });
 
 
-
 function validador()
 {
-	const miCbu = document.querySelector("#up_CBU");
-	miCbu.addEventListener("blur", validando);
-	miCbu.addEventListener("change", validando);
+	const miCbu = document.querySelector('#up_CBU');
+	miCbu.addEventListener('blur', validando);
+	miCbu.addEventListener('change', validando);
 	function validando()
 	{
 		if( miCbu.value.length === 22)
 		{
-			console.log("Todo correcto");
+			console.log('Todo correcto');
 		}
 		else
 		{
-			alert("El CBU debe tener 22 dígitos numéricos");
+			alert('El CBU debe tener 22 dígitos numéricos');
 		}
 	}
 }
