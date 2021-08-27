610cc7ef1ea1e4a7cb08be17c43ec17f2d775df6
Edgard_Roul <edgardo_raul@hotmail.com>
2021-08-27 17:03:19 -0300

    Suprimí los eliminadores de temporales del servidor.
    Evito así caídas en al funcionalidad del Lince.

--- Limpiador.bat
+++ Limpiador.bat
@@ -1,18 +1,5 @@
 @echo off
 rem "Elimina los archivos viejos para Producteca."
 del /S /F /Q D:\Producteca\Archivos\*.*
-
 del /S /F /Q C:\Users\admin\AppData\Local\Temp\*.*
-del /S /F /Q C:\Users\Admin2\AppData\Local\Temp\*.*
-del /S /F /Q C:\Users\Administrador\AppData\Local\Temp\*.*
-del /S /F /Q C:\Users\Agencia\AppData\Local\Temp\*.*
-del /S /F /Q C:\Users\Auditoria\AppData\Local\Temp\*.*
-del /S /F /Q C:\Users\Deposito\AppData\Local\Temp\*.*
-del /S /F /Q C:\Users\electronica\AppData\Local\Temp\*.*
-del /S /F /Q C:\Users\Elena\AppData\Local\Temp\*.*
-del /S /F /Q C:\Users\Presidente\AppData\Local\Temp\*.*
-del /S /F /Q C:\Users\taller\AppData\Local\Temp\*.*
-del /S /F /Q C:\Users\test\AppData\Local\Temp\*.*
-
-
 del /S /F /Q C:\Windows\Temp\*.*
