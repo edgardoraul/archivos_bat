6d5dc87fbc14cfa5e85448f14ce0b71a826c26f3
Edgard_Roul <edgardo_raul@hotmail.com>
2021-10-21 14:50:00 -0300

    Acomodar y limpiar basura del gitignore

--- Subidor.bat
+++ Subidor.bat
@@ -1,12 +1,13 @@
 @echo off
-echo user quecarajoteimporta@notengoidea.com.ar>> ftpcmd.dat
-echo suckmydick.please>> ftpcmd.dat
+echo user web@rerda.com>> ftpcmd.dat
+echo BjC4)8UJ5p;jUKau-l>> ftpcmd.dat
 echo bin>> ftpcmd.dat
 echo cd upload/>> ftpcmd.dat
 echo delete price.csv>> ftpcmd.dat
 echo delete stock.csv>> ftpcmd.dat
 echo put D:\Dropbox\Aplicaciones\Producteca\price.csv>> ftpcmd.dat
 echo put D:\Dropbox\Aplicaciones\Producteca\stock.csv>> ftpcmd.dat
+
 echo quit>> ftpcmd.dat
 ftp -n -s:ftpcmd.dat ftp.rerda.com
 del ftpcmd.dat
